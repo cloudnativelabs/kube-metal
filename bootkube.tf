24b941fd48f3cda877958d33fcaa26fc83db8bd8
bzub <bzubrod@gmail.com>
2017-09-17 22:33:32 -0500

    Pass all controller hostnames to bootkube-terraform module Previously only the first controller was added.

--- bootkube.tf
+++ bootkube.tf
@@ -1,15 +1,13 @@
 module "bootkube" {
-  source = "./modules/bootkube"
-
-  cluster_name = "${var.cluster_name}"
-
-  #TODO: Dynamically determine apiservers from nodes, set to a variable
-  api_servers                   = ["${packet_device.controller.0.hostname}"]
+  source                        = "./modules/bootkube"
+  cluster_name                  = "${var.cluster_name}"
+  api_servers                   = "${packet_device.controller.*.hostname}"
   asset_dir                     = "${var.asset_dir}"
   etcd_servers                  = ["${var.etcd_servers}"]
   experimental_self_hosted_etcd = "${var.experimental_self_hosted_etcd}"
-  container_images              = {
-                                    hyperkube = "quay.io/coreos/hyperkube:v${null_resource.kubernetes_facts.triggers.kubernetes_v_patch}_coreos.0"
-                                    etcd = "quay.io/coreos/etcd:v3.1.8"
-                                  }
+
+  container_images = {
+    hyperkube = "quay.io/coreos/hyperkube:v${null_resource.kubernetes_facts.triggers.kubernetes_v_patch}_coreos.0"
+    etcd      = "quay.io/coreos/etcd:v3.1.8"
+  }
 }
